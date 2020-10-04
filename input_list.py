0158b6109e95d99f63388bfbf2e79389b0c29bae
ProdexOne <69591862+ProdexOne@users.noreply.github.com>
2020-10-04 23:44:21 +0530

    Reduced code to 1 line which is more efficient.
    Used split() function.

--- input_list.py
+++ input_list.py
@@ -5,12 +5,4 @@
 # 1 2 3 4 5
 # Output list: [1, 2, 3, 4, 5]
 
-
-list = []                           #Initialising an empty list
-n = int(input('Input N: '))
-for i in range(0,n):                #To read the elements entered by the user
-    element = int(input())          #Reads all the elements entered by the user and stores it in 'element'
-
-    list.append(element)            #Adding the read elements into the empty list initialised in the beginning  
-
-print(list)                         #Prints the final list
+print(input().split())    # Taking input and using split function
